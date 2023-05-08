c7bdb1ee072f62f4783445d8fad5c97e63a71358
cwchilvers <cwchilvers@gmail.com>
2023-05-08 13:55:59 -0700

    added header for 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
-const APIkey = "41a901896f60f480de1298be37bde3f8"
+// OpenWeather
+const APIkey = "41a901896f60f480de1298be37bde3f8";
 var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={' + APIkey + '}';
+
+// Strings
+const symbols = {
+    cloudy: "☁️",
+    fog: "🌫️",
+    lightning: "🌩️",
+    partlyCloudy: "⛅",
+    rain: "🌧️",
+    snow: "❄️",
+    sun: "☀️",
+    tornado: "🌪️",
+};
 
 fetch(requestUrl)
     .then(function (response) {
     return response.json();
-})
+});
